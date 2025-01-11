bcc863803023ec0bd726f17f6eca2e5caa7045eb
Axwabo <axwabo@gmail.com>
2025-01-11 21:57:20 +0100

    PersonalizedAudioPlayer stub

---
+++ SecretLabNAudio.Core/PersonalizedAudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SecretLabNAudio.Core;
+
+public class PersonalizedAudioPlayer : TargetedAudioPlayer
+{
+
+    private readonly Dictionary<string, AudioPlayerSettings> _settingsPerUserId = [];
+
+    public void Override(ReferenceHub hub, AudioPlayerSettings settings)
+    {
+        _settingsPerUserId[hub.authManager.UserId] = settings;
+        Sync(hub);
+    }
+
+    public void Modify(ReferenceHub hub, Func<AudioPlayerSettings, AudioPlayerSettings> settingsTransform, Func<AudioPlayerSettings>? defaultSupplier = null)
+    {
+        // TODO
+    }
+
+    public void ClearOverride(ReferenceHub hub)
+    {
+        if (_settingsPerUserId.Remove(hub.authManager.UserId))
+            Sync(hub);
+    }
+
+    private void Sync(ReferenceHub hub)
+    {
+        // TODO: send fake sync vars
+    }
+
+}
