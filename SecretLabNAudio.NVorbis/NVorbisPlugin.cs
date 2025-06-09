35f03e331818cf8a9c45f11418f110125b21055c
Axwabo <axwabo@gmail.com>
2025-06-09 12:02:07 +0200

    vorbis support + auto-reference core

---
+++ SecretLabNAudio.NVorbis/NVorbisPlugin.cs
@@ -0,0 +1,23 @@
+﻿using LabApi.Loader.Features.Plugins;
+using SecretLabNAudio.Core.FileReading;
+
+namespace SecretLabNAudio.NVorbis;
+
+public sealed class NVorbisPlugin : Plugin
+{
+
+    public override string Name => "SecretLabNAudio.NVorbis";
+    public override string Description => "Registers NVorbis support";
+    public override string Author => "Axwabo";
+    public override Version Version => GetType().Assembly.GetName().Version;
+    public override Version RequiredApiVersion { get; } = new(1, 0, 0);
+
+    public static void RegisterFactory() => AudioReaderFactoryManager.RegisterFactory("ogg", new VorbisStreamFactory());
+
+    public override void Enable() => RegisterFactory();
+
+    public override void Disable()
+    {
+    }
+
+}
---
+++ SecretLabNAudio.NVorbis/VorbisStreamFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using NAudio.Vorbis;
+using SecretLabNAudio.Core.FileReading;
+
+namespace SecretLabNAudio.NVorbis;
+
+internal sealed class VorbisStreamFactory : IAudioReaderFactory
+{
+
+    private static AudioReaderFactoryResult Result(VorbisWaveReader reader)
+        => new(reader, reader);
+
+    public AudioReaderFactoryResult FromPath(string path) => Result(new VorbisWaveReader(path));
+
+    public AudioReaderFactoryResult FromStream(Stream stream, bool closeOnDispose) => Result(new VorbisWaveReader(stream, closeOnDispose));
+
+}
