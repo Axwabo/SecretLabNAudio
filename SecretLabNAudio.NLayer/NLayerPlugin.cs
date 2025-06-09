e32c46a6fe97547efdbae12929dba8c95ebe2518
Axwabo <axwabo@gmail.com>
2025-06-09 11:39:04 +0200

    NLayer support

---
+++ SecretLabNAudio.NLayer/NLayerPlugin.cs
@@ -0,0 +1,23 @@
+﻿using LabApi.Loader.Features.Plugins;
+using SecretLabNAudio.Core.FileReading;
+
+namespace SecretLabNAudio.NLayer;
+
+public sealed class NLayerPlugin : Plugin
+{
+
+    public override string Name => "SecretLabNAudio.NLayer";
+    public override string Description => "Registers NLayer support";
+    public override string Author => "Axwabo";
+    public override Version Version => GetType().Assembly.GetName().Version;
+    public override Version RequiredApiVersion { get; } = new(1, 0, 0);
+
+    public static void RegisterFactory() => AudioReaderFactoryManager.RegisterFactory("mp3", new MpegStreamFactory());
+
+    public override void Enable() => RegisterFactory();
+
+    public override void Disable()
+    {
+    }
+
+}
