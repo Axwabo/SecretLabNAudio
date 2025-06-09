6fca30d078b2f5bbf734e8b97efc3e042c1c96f8
Axwabo <axwabo@gmail.com>
2025-06-09 12:23:23 +0200

    media foundation support

---
+++ SecretLabNAudio.MediaFoundation/MediaFoundationFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using NAudio.Wave;
+using SecretLabNAudio.Core.FileReading;
+
+namespace SecretLabNAudio.MediaFoundation;
+
+internal sealed class MediaFoundationFactory : IAudioReaderFactory
+{
+
+    public AudioReaderFactoryResult FromPath(string path) => new MediaFoundationReader(path);
+
+    public AudioReaderFactoryResult FromStream(Stream stream, bool closeOnDispose) => new DisposableMediaFoundationReader(stream, closeOnDispose);
+
+}
+
+file sealed class DisposableMediaFoundationReader : StreamMediaFoundationReader
+{
+
+    private readonly ConditionalOneShotDisposable _disposable;
+
+    public DisposableMediaFoundationReader(Stream stream, bool closeOnDispose) : base(stream)
+        => _disposable = new ConditionalOneShotDisposable(stream, closeOnDispose);
+
+    protected override void Dispose(bool disposing)
+    {
+        _disposable.Dispose(disposing);
+        base.Dispose(disposing);
+    }
+
+}
---
+++ SecretLabNAudio.MediaFoundation/MediaFoundationPlugin.cs
@@ -0,0 +1,50 @@
+﻿using LabApi.Loader.Features.Plugins;
+using SecretLabNAudio.Core.FileReading;
+
+namespace SecretLabNAudio.MediaFoundation;
+
+public sealed class MediaFoundationPlugin : Plugin
+{
+
+    private static readonly string[] SupportedFormats =
+    [
+        ".3g2",
+        ".3gp",
+        ".3gp2",
+        ".3gpp",
+        ".asf",
+        ".wma",
+        ".wmv",
+        ".aac",
+        ".adts",
+        ".avi",
+        ".mp3",
+        ".m4a",
+        ".m4v",
+        ".mov",
+        ".mp4",
+        ".sami",
+        ".smi",
+        ".wav"
+    ];
+
+    public override string Name => "SecretLabNAudio.MediaFoundation";
+    public override string Description => "Registers Media Foundation support";
+    public override string Author => "Axwabo";
+    public override Version Version => GetType().Assembly.GetName().Version;
+    public override Version RequiredApiVersion { get; } = new(1, 0, 0);
+
+    public static void RegisterFactory()
+    {
+        var factory = new MediaFoundationFactory();
+        foreach (var format in SupportedFormats)
+            AudioReaderFactoryManager.RegisterFactory(format, factory);
+    }
+
+    public override void Enable() => RegisterFactory();
+
+    public override void Disable()
+    {
+    }
+
+}
