c4332d972e169bf7e32b34a8b415db3d41cf1841
Axwabo <axwabo@gmail.com>
2025-06-11 09:04:01 +0200

    plugin docs

--- SecretLabNAudio.MediaFoundation/MediaFoundationPlugin.cs
+++ SecretLabNAudio.MediaFoundation/MediaFoundationPlugin.cs
@@ -1,12 +1,15 @@
-﻿using LabApi.Loader.Features.Plugins;
+﻿using System.Collections.Generic;
+using LabApi.Loader.Features.Plugins;
 using SecretLabNAudio.Core.FileReading;
 
 namespace SecretLabNAudio.MediaFoundation;
 
+/// <summary>A plugin that registers Media Foundation support for SecretLabNAudio.Core. This plugin is Windows-specific.</summary>
 public sealed class MediaFoundationPlugin : Plugin
 {
 
-    private static readonly string[] SupportedFormats =
+    /// <summary>The file formats supported by the <see cref="MediaFoundationReader"/>.</summary>
+    public static IReadOnlyCollection<string> SupportedFormats { get; } =
     [
         ".3g2",
         ".3gp",
@@ -27,12 +30,23 @@ public sealed class MediaFoundationPlugin : Plugin
         ".smi"
     ];
 
+    /// <inheritdoc/>
     public override string Name => "SecretLabNAudio.MediaFoundation";
+
+    /// <inheritdoc/>
     public override string Description => "Registers Media Foundation support";
+
+    /// <inheritdoc/>
     public override string Author => "Axwabo";
+
+    /// <inheritdoc/>
     public override Version Version => GetType().Assembly.GetName().Version;
+
+    /// <inheritdoc/>
     public override Version RequiredApiVersion { get; } = new(1, 0, 0);
 
+    /// <summary>Registers a factory to read audio from files and other streams using Media Foundation.</summary>
+    /// <seealso cref="SupportedFormats"/>
     public static void RegisterFactory()
     {
         Ensure<MediaFoundationReader>();
@@ -43,8 +57,10 @@ public sealed class MediaFoundationPlugin : Plugin
 
     private static void Ensure<T>() => _ = typeof(T);
 
+    /// <summary>Calls <see cref="RegisterFactory"/>.</summary>
     public override void Enable() => RegisterFactory();
 
+    /// <inheritdoc/>
     public override void Disable()
     {
     }
