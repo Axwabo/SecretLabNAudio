c1bf24329257f5fe7de81807d86eb46cd7ae638f
Axwabo <axwabo@gmail.com>
2025-08-17 21:41:31 +0200

    case-insensitive AudioReaderFactoryManager

--- SecretLabNAudio.Core/FileReading/AudioReaderFactoryManager.cs
+++ SecretLabNAudio.Core/FileReading/AudioReaderFactoryManager.cs
@@ -7,7 +7,7 @@ namespace SecretLabNAudio.Core.FileReading;
 public static class AudioReaderFactoryManager
 {
 
-    private static readonly Dictionary<string, IAudioReaderFactory> Factories = [];
+    private static readonly Dictionary<string, IAudioReaderFactory> Factories = new(StringComparer.OrdinalIgnoreCase);
 
     static AudioReaderFactoryManager()
     {
@@ -19,34 +19,32 @@ public static class AudioReaderFactoryManager
         RegisterFactory("aif", aiff);
     }
 
-    private static string Sanitize(this string fileType) => fileType.TrimStart('.').ToLower();
-
     /// <summary>
     /// Registers an <see cref="IAudioReaderFactory"/> for the given file type.
     /// </summary>
-    /// <param name="fileType">The file type to register the factory for, e.g. "wav", "aiff".</param>
+    /// <param name="fileType">The file type to register the factory for, e.g. ".wav", "aiff".</param>
     /// <param name="factory">The factory to register.</param>
-    /// <remarks>The period is automatically trimmed from the start.</remarks>
+    /// <remarks>The period is automatically trimmed from the start. File type is case-insensitive.</remarks>
     public static void RegisterFactory(string fileType, IAudioReaderFactory factory)
-        => Factories[fileType.Sanitize()] = factory;
+        => Factories[fileType.TrimStart('.')] = factory;
 
     /// <summary>
     /// Attempts to get the <see cref="IAudioReaderFactory"/> for the given file type.
     /// </summary>
-    /// <param name="fileType">The file type to get the factory for, e.g. "wav", "aiff".</param>
+    /// <param name="fileType">The file type to get the factory for, e.g. ".wav", "aiff".</param>
     /// <param name="factory">The factory found for the file type, or <see langword="null"/> if no factory was found.</param>
     /// <returns>Whether a factory was found for the given file type.</returns>
-    /// <remarks>The period is automatically trimmed from the start.</remarks>
+    /// <remarks>The period is automatically trimmed from the start. File type is case-insensitive.</remarks>
     public static bool TryGetFactory(string fileType, [NotNullWhen(true)] out IAudioReaderFactory? factory)
-        => Factories.TryGetValue(fileType.Sanitize(), out factory);
+        => Factories.TryGetValue(fileType.TrimStart('.'), out factory);
 
     /// <summary>
     /// Gets the <see cref="IAudioReaderFactory"/> for the given file type.
     /// </summary>
-    /// <param name="fileType">The file type to get the factory for, e.g. "wav", "aiff".</param>
+    /// <param name="fileType">The file type to get the factory for, e.g. ".wav", "aiff".</param>
     /// <returns>The factory for the given file type.</returns>
     /// <exception cref="NotSupportedException">Thrown when no factory is registered for the given file type.</exception>
-    /// <remarks>The period is automatically trimmed from the start.</remarks>
+    /// <remarks>The period is automatically trimmed from the start. File type is case-insensitive.</remarks>
     public static IAudioReaderFactory GetFactory(string fileType)
         => TryGetFactory(fileType, out var factory)
             ? factory
