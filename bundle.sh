514d5262e7d431070f088b85e7a08d2cace12bd7
Axwabo <axwabo@gmail.com>
2025-11-24 15:42:44 +0100

    simplify release workflow

---
+++ bundle.sh
@@ -0,0 +1,14 @@
+# must be executed in the build output directory
+
+mkdir SecretLabNAudio
+mkdir SecretLabNAudio/bin
+mkdir SecretLabNAudio/LICENSES
+
+mv SecretLabNAudio.dll SecretLabNAudio/bin
+ls | grep -E "^(NAudio|NLayer|NVorbis)" | xargs -I {0} mv {0} SecretLabNAudio/bin
+
+cp -r ../THIRD_PARTY_LICENSES SecretLabNAudio/LICENSES
+cp ../LICENSE SecretLabNAudio/LICENSES/SecretLabNAudio.txt
+
+cd SecretLabNAudio
+zip -r ../SecretLabNAudio.zip .
