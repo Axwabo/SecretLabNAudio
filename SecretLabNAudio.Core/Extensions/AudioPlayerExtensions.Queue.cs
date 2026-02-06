2086cff68d3afca9d55fd9147b0e16ad674ce087
Axwabo <axwabo@gmail.com>
2026-02-06 16:55:48 +0100

    remove loop from queue methods

--- SecretLabNAudio.Core/Extensions/AudioPlayerExtensions.Queue.cs
+++ SecretLabNAudio.Core/Extensions/AudioPlayerExtensions.Queue.cs
@@ -26,81 +26,75 @@ public static partial class AudioPlayerExtensions
         /// Enqueues a file to be played.
         /// </summary>
         /// <param name="path">The path to the file.</param>
-        /// <param name="loop">Whether to loop the file.</param>
         /// <param name="volume">The volume of the input.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Queue.xml' path='doc/Player/remarks'/>
         /// <include file='../XmlDocs/Files.xml' path='doc/exception'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.EnqueueFile"/>
-        public AudioPlayer EnqueueFile(string path, bool loop = false, float volume = 1)
-            => player.EnqueueFile(path, ModifyChain.AmplifyIfNot1(volume), loop);
+        public AudioPlayer EnqueueFile(string path, float volume = 1)
+            => player.EnqueueFile(path, ModifyChain.AmplifyIfNot1(volume));
 
         /// <summary>
         /// Enqueues a file to be played.
         /// </summary>
         /// <param name="path">The path to the file.</param>
         /// <param name="process">An optional <see cref="ModifyChain"/> specifying how to process the stream.</param>
-        /// <param name="loop">Whether to loop the file.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Mixer.xml' path='doc/Player/remarks'/>
         /// <include file='../XmlDocs/Files.xml' path='doc/exception'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.EnqueueFile"/>
-        public AudioPlayer EnqueueFile(string path, ModifyChain? process, bool loop = false)
-            => player.UseQueue(queue => queue.EnqueueFile(path, loop, process.Prepend(ProcessorChainExtensions.ToPlayerCompatible)));
+        public AudioPlayer EnqueueFile(string path, ModifyChain? process)
+            => player.UseQueue(queue => queue.EnqueueFile(path, process.Prepend(ProcessorChainExtensions.ToPlayerCompatible)));
 
         /// <summary>
         /// Attempts to enqueue a file to be played without throwing an exception if the file is not found or is not supported.
         /// </summary>
         /// <param name="path">The path to the file.</param>
-        /// <param name="loop">Whether to loop the file.</param>
         /// <param name="volume">The volume of the input.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Queue.xml' path='doc/Player/remarks'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.TryEnqueueFile"/>
-        public AudioPlayer EnqueueFileSafe(string path, bool loop = false, float volume = 1)
-            => player.EnqueueFileSafe(path, ModifyChain.AmplifyIfNot1(volume), loop);
+        public AudioPlayer EnqueueFileSafe(string path, float volume = 1)
+            => player.EnqueueFileSafe(path, ModifyChain.AmplifyIfNot1(volume));
 
         /// <summary>
         /// Attempts to enqueue a file to be played without throwing an exception if the file is not found or is not supported.
         /// </summary>
         /// <param name="path">The path to the file.</param>
         /// <param name="process">An optional <see cref="ModifyChain"/> specifying how to process the stream.</param>
-        /// <param name="loop">Whether to loop the file.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Queue.xml' path='doc/Player/remarks'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.TryEnqueueFile"/>
-        public AudioPlayer EnqueueFileSafe(string path, ModifyChain? process, bool loop = false)
-            => player.UseQueue(queue => queue.TryEnqueueFile(path, loop, process.Prepend(ProcessorChainExtensions.ToPlayerCompatible)));
+        public AudioPlayer EnqueueFileSafe(string path, ModifyChain? process)
+            => player.UseQueue(queue => queue.TryEnqueueFile(path, process.Prepend(ProcessorChainExtensions.ToPlayerCompatible)));
 
         /// <summary>
         /// Enqueues a short clip to be played (if a clip with the given name was registered).
         /// </summary>
         /// <param name="name">The name of the clip.</param>
-        /// <param name="loop">Whether to loop the clip.</param>
         /// <param name="volume">The volume of the input.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Queue.xml' path='doc/Player/remarks'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.EnqueueShortClip"/>
-        public AudioPlayer EnqueueShortClip(ClipName name, bool loop = false, float volume = 1)
-            => player.EnqueueShortClip(name, ModifyChain.AmplifyIfNot1(volume), loop);
+        public AudioPlayer EnqueueShortClip(ClipName name, float volume = 1)
+            => player.EnqueueShortClip(name, ModifyChain.AmplifyIfNot1(volume));
 
         /// <summary>
         /// Enqueues a short clip to be played (if a clip with the given name was registered).
         /// </summary>
         /// <param name="name">The name of the clip.</param>
         /// <param name="process">An optional <see cref="ModifyChain"/> specifying how to process the clip.</param>
-        /// <param name="loop">Whether to loop the clip.</param>
         /// <returns>The player itself.</returns>
         /// <include file='../XmlDocs/Queue.xml' path='doc/Player/remarks'/>
         /// <seealso cref="UseQueue(AudioPlayer,Action{AudioQueue})"/>
         /// <seealso cref="AudioQueueExtensions.EnqueueShortClip"/>
-        public AudioPlayer EnqueueShortClip(ClipName name, ModifyChain? process, bool loop = false)
-            => player.UseQueue(queue => queue.EnqueueShortClip(name, loop, process));
+        public AudioPlayer EnqueueShortClip(ClipName name, ModifyChain? process)
+            => player.UseQueue(queue => queue.EnqueueShortClip(name, process));
 
     }
 
