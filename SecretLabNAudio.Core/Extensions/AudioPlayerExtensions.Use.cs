b6a6df7d730dbbe361743af165c88ba83352a345
Axwabo <axwabo@gmail.com>
2026-02-07 18:34:19 +0100

    fix word repeat

--- SecretLabNAudio.Core/Extensions/AudioPlayerExtensions.Use.cs
+++ SecretLabNAudio.Core/Extensions/AudioPlayerExtensions.Use.cs
@@ -80,7 +80,7 @@ public static partial class AudioPlayerExtensions
                 : player;
 
         /// <summary>
-        /// If the <see cref="SingleInputAs">single input</see> input is not an <see cref="AudioQueue"/>,
+        /// If the <see cref="SingleInputAs">single input</see> is not an <see cref="AudioQueue"/>,
         /// replaces the <see cref="AudioPlayer.SampleProvider"/> with a new one.
         /// </summary>
         /// <returns>The player itself.</returns>
@@ -95,7 +95,7 @@ public static partial class AudioPlayerExtensions
         }
 
         /// <summary>
-        /// If the <see cref="SingleInputAs">single input</see> input is not an <see cref="AudioQueue"/>,
+        /// If the <see cref="SingleInputAs">single input</see> is not an <see cref="AudioQueue"/>,
         /// replaces the <see cref="AudioPlayer.SampleProvider"/> with a new one.
         /// </summary>
         /// <param name="queue">A delegate to add items to the queue with.</param>
