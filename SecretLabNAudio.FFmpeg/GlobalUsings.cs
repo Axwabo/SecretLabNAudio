9218b96965bb73f248524d1947ea83cbb4b5d25a
Axwabo <axwabo@gmail.com>
2026-04-01 08:25:10 +0200

    explicit error name

--- SecretLabNAudio.FFmpeg/GlobalUsings.cs
+++ SecretLabNAudio.FFmpeg/GlobalUsings.cs
@@ -4,4 +4,4 @@ global using SecretLabNAudio.Core;
 global using SecretLabNAudio.FFmpeg.Interop;
 global using Logger = LabApi.Features.Console.Logger;
 global using Debug = UnityEngine.Debug;
-global using SaveCacheResult = (string OutputPath, SecretLabNAudio.FFmpeg.Caches.SaveCacheError?);
+global using SaveCacheResult = (string OutputPath, SecretLabNAudio.FFmpeg.Caches.SaveCacheError? Error);
