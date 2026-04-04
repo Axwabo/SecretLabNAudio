6af089fda1195cc439c749c00743fc11da9e232b
Axwabo <axwabo@gmail.com>
2026-04-04 11:32:51 +0200

    use env

--- SecretLabNAudio.FFmpeg/Installer/FFmpegInstaller.Linux.cs
+++ SecretLabNAudio.FFmpeg/Installer/FFmpegInstaller.Linux.cs
@@ -6,7 +6,7 @@ public static partial class FFmpegInstaller
     private const string LinuxUrl = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-lgpl.tar.xz";
     private const string LinuxArchive = "ffmpeg.tar.xz";
     private const string LinuxDecompress = $"tar -xf {LinuxArchive} --strip=2 --overwrite --wildcards */bin/*";
-    private const string Bash = "/usr/bin/bash";
+    private const string Env = "/usr/bin/bash";
     private const string LinuxExecutable = "ffmpeg";
 
     private static async Awaitable<string?> InstallLinux()
@@ -14,7 +14,7 @@ public static partial class FFmpegInstaller
         Logger.Info("Downloading FFmpeg from BtbN builds...");
         await Download(LinuxUrl, Path.Combine(Folder, LinuxArchive));
         Logger.Info("Extracting FFmpeg...");
-        var (decompressed, tarError) = Execute(Bash, $"-c \"{LinuxDecompress}\"");
+        var (decompressed, tarError) = Execute(Env, $"bash -c \"{LinuxDecompress}\"");
         if (!decompressed)
         {
             Logger.Error($"Failed to extract FFmpeg:\n{tarError ?? "could not start \"tar\" process via bash"}");
@@ -32,8 +32,8 @@ public static partial class FFmpegInstaller
         if (!File.Exists(FFmpegSL.Path))
             return (false, "The configured FFmpeg installation does not exist or is not a file that can be made executable.");
         var (changed, chmodError) = Execute(
-            Bash,
-            $"-c \"chmod +x '{Path.GetFileName(FFmpegSL.Path)}'\"",
+            Env,
+            $"chmod +x \"{Path.GetFileName(FFmpegSL.Path)}\"",
             Path.GetFullPath(Path.GetDirectoryName(FFmpegSL.Path) ?? FFmpegSL.Path)
         );
         return changed
