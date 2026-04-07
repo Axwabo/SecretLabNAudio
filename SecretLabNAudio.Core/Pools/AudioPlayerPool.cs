8e9ac9404bab2c12f368bcca6eb5d34ba6b68f6d
Axwabo <axwabo@gmail.com>
2026-04-07 09:47:08 +0200

    shared APPool docs

--- SecretLabNAudio.Core/Pools/AudioPlayerPool.cs
+++ SecretLabNAudio.Core/Pools/AudioPlayerPool.cs
@@ -15,11 +15,7 @@ public static class AudioPlayerPool
     /// <summary>
     /// Rents an <see cref="AudioPlayer"/> from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// </summary>
-    /// <param name="id">The controller ID to assign to the player.</param>
-    /// <param name="settings">The settings to apply to the player.</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
     public static AudioPlayer Rent(byte id, SpeakerSettings settings, Transform? parent = null, Vector3 position = default, bool spawn = true)
     {
@@ -36,12 +32,12 @@ public static class AudioPlayerPool
     /// <summary>
     /// Rents an <see cref="AudioPlayer"/> with the next available ID from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// </summary>
-    /// <param name="settings">The settings to apply to the player.</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="settings"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="parent"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="position"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="spawn"]'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
-    /// <exception cref="OverflowException">Thrown when no IDs are available.</exception>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/exception'/>
     /// <seealso cref="NextAvailableId"/>
     public static AudioPlayer Rent(SpeakerSettings settings, Transform? parent = null, Vector3 position = default, bool spawn = true)
         => Rent(NextAvailableId, settings, parent, position, spawn);
@@ -50,10 +46,10 @@ public static class AudioPlayerPool
     /// Rents an <see cref="AudioPlayer"/> with the next available ID from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// Applies <see cref="SpeakerSettings.Default"/>.
     /// </summary>
-    /// <param name="id">The controller ID to assign to the player.</param>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="id"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="position"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="parent"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="spawn"]'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
     /// <seealso cref="NextAvailableId"/>
     public static AudioPlayer RentDefault(byte id, Vector3 position = default, Transform? parent = null, bool spawn = true)
@@ -63,11 +59,11 @@ public static class AudioPlayerPool
     /// Rents an <see cref="AudioPlayer"/> with the next available ID from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// Applies <see cref="SpeakerSettings.Default"/>.
     /// </summary>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="position"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="parent"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="spawn"]'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
-    /// <exception cref="OverflowException">Thrown when no IDs are available.</exception>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/exception'/>
     /// <seealso cref="NextAvailableId"/>
     public static AudioPlayer RentDefault(Vector3 position = default, Transform? parent = null, bool spawn = true)
         => Rent(NextAvailableId, SpeakerSettings.Default, parent, position, spawn);
@@ -76,10 +72,10 @@ public static class AudioPlayerPool
     /// Rents an <see cref="AudioPlayer"/> with the next available ID from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// Applies <see cref="SpeakerSettings.GloballyAudible"/>.
     /// </summary>
-    /// <param name="id">The controller ID to assign to the player.</param>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="id"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="position"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="parent"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="spawn"]'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
     /// <seealso cref="NextAvailableId"/>
     public static AudioPlayer RentGloballyAudible(byte id, Vector3 position = default, Transform? parent = null, bool spawn = true)
@@ -89,11 +85,11 @@ public static class AudioPlayerPool
     /// Rents an <see cref="AudioPlayer"/> with the next available ID from the pool, or creates a new one if no <see cref="SpeakerToy"/> is pooled.
     /// Applies <see cref="SpeakerSettings.GloballyAudible"/>.
     /// </summary>
-    /// <param name="position">The position of the speaker in local space (world space if no parent is specified).</param>
-    /// <param name="parent">The <see cref="Transform"/> to parent the player to. <see langword="null"/> if it should not be parented.</param>
-    /// <param name="spawn">Whether to invoke <see cref="NetworkServer.Spawn(GameObject,NetworkConnection)"/>.</param>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="position"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="parent"]'/>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/param[@name="spawn"]'/>
     /// <returns>A new or reused <see cref="AudioPlayer"/>.</returns>
-    /// <exception cref="OverflowException">Thrown when no IDs are available.</exception>
+    /// <include file='../XmlDocs/Pools.xml' path='doc/Player/exception'/>
     /// <seealso cref="NextAvailableId"/>
     public static AudioPlayer RentGloballyAudible(Vector3 position = default, Transform? parent = null, bool spawn = true)
         => Rent(NextAvailableId, SpeakerSettings.GloballyAudible, parent, position, spawn);
