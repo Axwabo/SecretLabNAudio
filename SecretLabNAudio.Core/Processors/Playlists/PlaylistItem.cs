5bc8b4c57a3072e64358119b9a35ab05767622f6
Axwabo <axwabo@gmail.com>
2026-05-02 17:09:31 +0200

    more docs

--- SecretLabNAudio.Core/Processors/Playlists/PlaylistItem.cs
+++ SecretLabNAudio.Core/Processors/Playlists/PlaylistItem.cs
@@ -3,31 +3,47 @@ using SecretLabNAudio.Core.FileReading;
 
 namespace SecretLabNAudio.Core.Processors.Playlists;
 
+/// <summary>
+/// Represents a queued item in a <see cref="LazyPlaylist"/>.
+/// </summary>
+/// <param name="Name">The name of the item.</param>
 public abstract record PlaylistItem(string? Name = null)
 {
 
-    public abstract ISampleProvider CreateProvider(int sampleRate, int channels);
+    /// <summary>
+    /// Creates a sample provider for the playlist item.
+    /// </summary>
+    /// <param name="sampleRate">The sample rate of the playlist.</param>
+    /// <param name="channels">The channel count of the playlist.</param>
+    /// <returns>A new sample provider based on the item.</returns>
+    /// <remarks>
+    /// The <paramref name="sampleRate"/> and <paramref name="channels"/> parameters are hints only, allowing the inheritor to pass those to an external resource.
+    /// <see cref="LazyPlaylist"/> will automatically convert the provider to its own format.
+    /// </remarks>
+    protected internal abstract ISampleProvider CreateProvider(int sampleRate, int channels);
 
 }
 
 public sealed record FilePlaylistItem(string FilePath) : PlaylistItem(Path.GetFileNameWithoutExtension(FilePath))
 {
 
-    public override ISampleProvider CreateProvider(int sampleRate, int channels) => CreateAudioProcessor.FromFile(FilePath);
+    protected internal override ISampleProvider CreateProvider(int sampleRate, int channels)
+        => CreateAudioProcessor.FromFile(FilePath);
 
 }
 
 public sealed record ShortClipPlaylistItem(ClipName ClipName) : PlaylistItem(ClipName.ToString())
 {
 
-    public override ISampleProvider CreateProvider(int sampleRate, int channels) => ShortClipCache.Get(ClipName);
+    protected internal override ISampleProvider CreateProvider(int sampleRate, int channels)
+        => ShortClipCache.Get(ClipName);
 
 }
 
 public sealed record ProcessedPlaylistItem(PlaylistItem Inner, ModifyChain Process) : PlaylistItem(Inner.Name)
 {
 
-    public override ISampleProvider CreateProvider(int sampleRate, int channels)
+    protected internal override ISampleProvider CreateProvider(int sampleRate, int channels)
     {
         var provider = Inner.CreateProvider(sampleRate, channels);
         var chain = ProviderToProcessor.SampleProviderToProcessor(provider, true).ToChain().ToFormat(sampleRate, channels);
--- SecretLabNAudio.FFmpeg/Processors/CachedPlaylistItem.cs
+++ SecretLabNAudio.FFmpeg/Processors/CachedPlaylistItem.cs
@@ -7,11 +7,18 @@ namespace SecretLabNAudio.FFmpeg.Processors;
 public abstract record CachedPlaylistItem<TSource, TKey>(TSource Source, string? Name = null) : PlaylistItem(Name)
 {
 
+    /// <summary>
+    /// Gets the cache instance to use for file lookups.
+    /// </summary>
+    /// <returns>A cache instance.</returns>
     protected abstract AudioCacheBase<TSource, TKey> GetCache();
 
+    /// <summary>Creates a sample provider when there was no cache hit.</summary>
+    /// <inheritdoc cref="PlaylistItem.CreateProvider"/>
     protected abstract ISampleProvider CreateFallback(int sampleRate, int channels);
 
-    public sealed override ISampleProvider CreateProvider(int sampleRate, int channels)
+    /// <inheritdoc/>
+    protected sealed override ISampleProvider CreateProvider(int sampleRate, int channels)
         => GetCache().TryGetPath(Source, out var path)
             ? CreateAudioProcessor.FromFile(path)
             : CreateFallback(sampleRate, channels);
@@ -21,8 +28,10 @@ public abstract record CachedPlaylistItem<TSource, TKey>(TSource Source, string?
 public sealed record CachedFilePlaylistItem(string FilePath) : CachedPlaylistItem<string, int>(FilePath, Path.GetFileNameWithoutExtension(FilePath))
 {
 
+    /// <inheritdoc/>
     protected override AudioCacheBase<string, int> GetCache() => SimpleFileCache.Shared;
 
+    /// <inheritdoc/>
     protected override ISampleProvider CreateFallback(int sampleRate, int channels)
         => CreateAudioProcessor.FromFile(FilePath);
 
